dc903aa296de9ad66f2387a0e198e0a27ef56557
sumrender <sumrenders@gmail.com>
2022-10-07 18:04:13 +0530

    add cors

--- backend/server.js
+++ backend/server.js
@@ -7,9 +7,12 @@ const { chats } = require("./dummy_data/data.js");
 const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
 const messageRoutes = require("./routes/messageRoute");
+const cors = require("cors");
+
 
 const PORT = process.env.PORT || 5000;
 const app = express();
+app.use(cors());
 
 app.use(express.json()); // to accept json data
 dotenv.config();
